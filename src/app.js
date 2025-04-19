370208d9db130290582f711d22b6aadbea5d0ea0
David <odilovrakhmidin.gmail.com>
2025-04-20 05:34:13 +0900

    fix: modify logics on app.js

--- src/app.js
+++ src/app.js
@@ -3,39 +3,36 @@ const {
   conversations,
   createConversation,
 } = require("@grammyjs/conversations");
-const Question = require("./schema/questionModel");
-const { assert } = require("console");
-const { afterEach } = require("node:test");
-
+const Question = require("./schema/QuestionModel");
 
 const bot = new Bot(process.env.TOKEN);
 bot.use(conversations());
 
-//adminMiddleware
-const adminMiddleware = async (input, next) => {
-  if (input.from?.id !== process.env.ADMIN) {
-    await input.reply("You are not Admin!");
+const adminMiddleware = async (ctx, next) => {
+  if (ctx.from?.id !== process.env.ADMIN) {
+    await ctx.reply("You are not admin to use this bot.");
     return;
   }
   return next();
 };
 
-bot.command("start", async (input) => {
-  await input.reply(`Welcome to chat box ${input.message.from.first_name}! 
+bot.command("start", async (ctx) => {
+  await ctx.reply(`Welcome to study up ${ctx.message.from.first_name}!
 
-    You can make questions with me by sending these commands!:
-
-    /newQueastion - make a question
-    /results - take your quizs`);
+You can control me by sending these commands:
+        
+/newword - create a new quiz
+/mywords - start your quizzes
+        `);
 });
 
-async function newWordQuestion(question, ctx) {
-  await ctx.reply("Send this format: `hello-salom:help-yordam;night-tun=10` ", {
+async function newWordConversation(conversation, ctx) {
+  await ctx.reply("This format send: `short-kichik:work-ish:late-kech=6`", {
     parse_mode: "Markdown",
   });
 
-  const messages = await conversation.wait();
-  const word = messages.message?.text;
+  const msg = await conversation.wait();
+  const word = msg.message?.text;
 
   if (word) {
     const target = await Question.findOne({
@@ -55,36 +52,36 @@ async function newWordQuestion(question, ctx) {
           question: word,
         });
       } catch (err) {
-        await ctx.reply("Something went wrong");
+        await ctx.reply("Something went wrong!");
       }
     }
-    await ctx.reply(`Successefully added. Take your quizes  /results`);
+    await ctx.reply(`Successfully add! Start your quizzes /mywords`);
   } else {
     await ctx.reply("This format sending please!");
   }
 }
 
-bot.use(createConversation(newWordQuestion));
+bot.use(createConversation(newWordConversation));
 
-bot.command("newWord", async (ctx) => {
-  await ctx.conversation.enter("newWordQuestion");
+bot.command("newword", async (ctx) => {
+  await ctx.conversation.enter("newWordConversation");
 });
 
-bot.command("myresults", async (ctx) => {
+bot.command("mywords", async (ctx) => {
   const result = await Question.findOne({
     memberId: String(ctx.from?.id),
   }).exec();
 
   if (result) {
     const [pairStr, delayStr] = result.question.split("=");
-    const delay = perseInt(delayStr) * 3000;
+    const delay = parseInt(delayStr) * 1000;
 
-    const pairList = pairStr.split(":").map((pair) => pair.split(":"));
+    const pairList = pairStr.split(":").map((pair) => pair.split("-"));
 
     function shuffleArray(arr) {
       const shuffled = [...arr];
       for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = math.floor(Math.random() * (i + 1));
+        const j = Math.floor(Math.random() * (i + 1));
         [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
       }
       return shuffled;
@@ -92,6 +89,7 @@ bot.command("myresults", async (ctx) => {
 
     const questions = pairList.map(([eng, uz], _, arr) => {
       let wrongOptions = arr.filter(([e]) => e !== eng).map(([_, u]) => u);
+
       wrongOptions = shuffleArray(wrongOptions).slice(0, 2);
       const options = shuffleArray([uz, ...wrongOptions, "i don't"]);
 
@@ -111,7 +109,7 @@ bot.command("myresults", async (ctx) => {
       setTimeout(async () => {
         try {
           if (previousPollMessageId) {
-            await ctx.api.deleteMyMessage(ctx.chat.id, previousPollMessageId);
+            await ctx.api.deleteMessage(ctx.chat.id, previousPollMessageId);
           }
 
           const sentPoll = await ctx.api.sendPoll(
@@ -131,25 +129,23 @@ bot.command("myresults", async (ctx) => {
 
           if (i === questions.length - 1) {
             setTimeout(() => {
-              ctx.reply("Question end! Tap to /results");
+              ctx.reply("Question end! Tap to /mywords");
             }, 2000);
           }
-        } catch (err) {
-          ctx.reply("Something went wrong! tap to /start");
+        } catch (error) {
+          ctx.reply("Something went wrong! Tap to /start");
         }
       }, i * delay);
     }
-  }
-  else {
-    await ctx.reply(`Not found your words!`)
+  } else {
+    await ctx.reply(`Not found your words!`);
   }
 });
 
-bot.command("admin", adminMiddleware, async (ctx)=> {
-    await ctx.reply("Welcome admin!")
+bot.command("admin", adminMiddleware, async (ctx) => {
+  await ctx.reply("Welcome admin!");
 });
 
-bot.catch((err) => {
-    console.log("Error in bot:", err)
-});
-module.exports = bot
+bot.catch((err) => console.error("Error in bot:", err));
+
+module.exports = bot;
